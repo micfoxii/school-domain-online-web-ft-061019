8c4d9a5a81bf83223dde0c945ee5abfc6531218c
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-13 19:35:42 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
+ require 'pry'
+ class School
+  attr_accessor :name, :roster
+  attr_reader :school_name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster 
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade].push(name)
+    else 
+      @roster[grade] = [name]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+   @roster.each do |grade, students|
+   students.sort!
+   end
+   end
+  
+end
 # code here!
